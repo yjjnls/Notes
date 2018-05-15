e71db18ff50675326d43986dd033fb03f65e2820
yjjnls <x-jj@foxmail.com>
2018-05-15 11:17:07 +0800

    update

--- opensource/tesseract/build-tesser.bat
+++ opensource/tesseract/build-tesser.bat
@@ -1,7 +1,9 @@
+cd tesseract
 mkdir build && pushd build
-cppan ../tesseract
-cmake ../tesseract -G "Visual Studio 14 2015 Win64" -DCMAKE_INSTALL_PREFIX="../out"
+cppan ..
+cmake .. -G "Visual Studio 14 2015 Win64" -DCMAKE_INSTALL_PREFIX="../../out"
 cmake --build . --target ALL_BUILD --config Release -- /p:Platform=x64  
 cmake --build . --target INSTALL   --config Release -- /p:Platform=x64 
 
 popd
+cd ..
