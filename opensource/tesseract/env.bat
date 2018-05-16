95e9c5af7bae17e8293ce986236ff84ff5f4ff84
yjjnls <x-jj@foxmail.com>
2018-05-16 18:29:36 +0800

    update

--- opensource/tesseract/env.bat
+++ opensource/tesseract/env.bat
@@ -1,1 +1,2 @@
 set PATH=D:\Working\MyProject\Notes\opensource\tesseract\out\bin;C:\gstreamer\1.0\x86_64\bin;C:\gstreamer\1.0\x86_64\lib\gstreamer-1.0;%PATH%
+set TESSDATA_PREFIX=C:\Program Files (x86)\Tesseract-OCR\tessdata
