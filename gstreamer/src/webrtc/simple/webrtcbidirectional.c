8179cb8ade7d47f298b0cda5debfb0cf1e4dc8d1
yjjnls <x-jj@foxmail.com>
2018-07-16 09:15:59 +0800

    update

--- gstreamer/src/webrtc/simple/webrtcbidirectional.c
+++ gstreamer/src/webrtc/simple/webrtcbidirectional.c
@@ -3,7 +3,6 @@
 #include <gst/webrtc/webrtc.h>
 
 #include <string.h>
-#include <stdio.h>
 
 static GMainLoop *loop;
 static GstElement *pipe1, *webrtc1, *webrtc2;
@@ -12,46 +11,48 @@ static GstBus *bus1;
 static gboolean
 _bus_watch(GstBus *bus, GstMessage *msg, GstElement *pipe)
 {
-    switch (GST_MESSAGE_TYPE(msg))
-    {
+    switch (GST_MESSAGE_TYPE(msg)) {
         case GST_MESSAGE_STATE_CHANGED:
-            if (GST_ELEMENT(msg->src) == pipe)
-            {
+            if (GST_ELEMENT(msg->src) == pipe) {
                 GstState old, new, pending;
 
                 gst_message_parse_state_changed(msg, &old, &new, &pending);
 
                 {
                     gchar *dump_name = g_strconcat("state_changed-",
-                                                   gst_element_state_get_name(old), "_",
-                                                   gst_element_state_get_name(new), NULL);
+                                                   gst_element_state_get_name(old),
+                                                   "_",
+                                                   gst_element_state_get_name(new),
+                                                   NULL);
                     GST_DEBUG_BIN_TO_DOT_FILE_WITH_TS(GST_BIN(msg->src),
-                                                      GST_DEBUG_GRAPH_SHOW_ALL, dump_name);
+                                                      GST_DEBUG_GRAPH_SHOW_ALL,
+                                                      dump_name);
                     g_free(dump_name);
                 }
             }
             break;
-        case GST_MESSAGE_ERROR:
-        {
+        case GST_MESSAGE_ERROR: {
             GError *err = NULL;
             gchar *dbg_info = NULL;
 
             GST_DEBUG_BIN_TO_DOT_FILE_WITH_TS(GST_BIN(pipe),
-                                              GST_DEBUG_GRAPH_SHOW_ALL, "error");
+                                              GST_DEBUG_GRAPH_SHOW_ALL,
+                                              "error");
 
             gst_message_parse_error(msg, &err, &dbg_info);
             g_printerr("ERROR from element %s: %s\n",
-                       GST_OBJECT_NAME(msg->src), err->message);
+                       GST_OBJECT_NAME(msg->src),
+                       err->message);
             g_printerr("Debugging info: %s\n", (dbg_info) ? dbg_info : "none");
             g_error_free(err);
             g_free(dbg_info);
             g_main_loop_quit(loop);
             break;
         }
-        case GST_MESSAGE_EOS:
-        {
+        case GST_MESSAGE_EOS: {
             GST_DEBUG_BIN_TO_DOT_FILE_WITH_TS(GST_BIN(pipe),
-                                              GST_DEBUG_GRAPH_SHOW_ALL, "eos");
+                                              GST_DEBUG_GRAPH_SHOW_ALL,
+                                              "eos");
             g_print("EOS received\n");
             g_main_loop_quit(loop);
             break;
@@ -74,8 +75,9 @@ _webrtc_pad_added(GstElement *webrtc, GstPad *new_pad, GstElement *pipe)
 
     out = gst_parse_bin_from_description(
         "rtpvp8depay ! vp8dec ! "
-        "videoconvert ! queue ! autovideosink sync=false",
-        TRUE, NULL);
+        "videoconvert ! queue ! xvimagesink",
+        TRUE,
+        NULL);
     gst_bin_add(GST_BIN(pipe), out);
     gst_element_sync_state_with_parent(out);
 
@@ -93,8 +95,7 @@ _on_answer_received(GstPromise *promise, gpointer user_data)
 
     g_assert(gst_promise_wait(promise) == GST_PROMISE_RESULT_REPLIED);
     reply = gst_promise_get_reply(promise);
-    gst_structure_get(reply, "answer",
-                      GST_TYPE_WEBRTC_SESSION_DESCRIPTION, &answer, NULL);
+    gst_structure_get(reply, "answer", GST_TYPE_WEBRTC_SESSION_DESCRIPTION, &answer, NULL);
     gst_promise_unref(promise);
     desc = gst_sdp_message_as_text(answer->sdp);
     g_print("Created answer:\n%s\n", desc);
@@ -122,8 +123,7 @@ _on_offer_received(GstPromise *promise, gpointer user_data)
 
     g_assert(gst_promise_wait(promise) == GST_PROMISE_RESULT_REPLIED);
     reply = gst_promise_get_reply(promise);
-    gst_structure_get(reply, "offer",
-                      GST_TYPE_WEBRTC_SESSION_DESCRIPTION, &offer, NULL);
+    gst_structure_get(reply, "offer", GST_TYPE_WEBRTC_SESSION_DESCRIPTION, &offer, NULL);
     gst_promise_unref(promise);
     desc = gst_sdp_message_as_text(offer->sdp);
     g_print("Created offer:\n%s\n", desc);
@@ -132,8 +132,7 @@ _on_offer_received(GstPromise *promise, gpointer user_data)
     g_signal_emit_by_name(webrtc1, "set-local-description", offer, NULL);
     g_signal_emit_by_name(webrtc2, "set-remote-description", offer, NULL);
 
-    promise = gst_promise_new_with_change_func(_on_answer_received, user_data,
-                                               NULL);
+    promise = gst_promise_new_with_change_func(_on_answer_received, user_data, NULL);
     g_signal_emit_by_name(webrtc2, "create-answer", NULL, promise);
 
     gst_webrtc_session_description_free(offer);
@@ -144,14 +143,12 @@ _on_negotiation_needed(GstElement *element, gpointer user_data)
 {
     GstPromise *promise;
 
-    promise = gst_promise_new_with_change_func(_on_offer_received, user_data,
-                                               NULL);
+    promise = gst_promise_new_with_change_func(_on_offer_received, user_data, NULL);
     g_signal_emit_by_name(webrtc1, "create-offer", NULL, promise);
 }
 
 static void
-_on_ice_candidate(GstElement *webrtc, guint mlineindex, gchar *candidate,
-                  GstElement *other)
+_on_ice_candidate(GstElement *webrtc, guint mlineindex, gchar *candidate, GstElement *other)
 {
     g_signal_emit_by_name(other, "add-ice-candidate", mlineindex, candidate);
 }
@@ -172,17 +169,12 @@ int main(int argc, char *argv[])
     gst_bus_add_watch(bus1, (GstBusFunc)_bus_watch, pipe1);
 
     webrtc1 = gst_bin_get_by_name(GST_BIN(pipe1), "smpte");
-    g_signal_connect(webrtc1, "on-negotiation-needed",
-                     G_CALLBACK(_on_negotiation_needed), NULL);
-    g_signal_connect(webrtc1, "pad-added", G_CALLBACK(_webrtc_pad_added),
-                     pipe1);
+    g_signal_connect(webrtc1, "on-negotiation-needed", G_CALLBACK(_on_negotiation_needed), NULL);
+    g_signal_connect(webrtc1, "pad-added", G_CALLBACK(_webrtc_pad_added), pipe1);
     webrtc2 = gst_bin_get_by_name(GST_BIN(pipe1), "ball");
-    g_signal_connect(webrtc2, "pad-added", G_CALLBACK(_webrtc_pad_added),
-                     pipe1);
-    g_signal_connect(webrtc1, "on-ice-candidate",
-                     G_CALLBACK(_on_ice_candidate), webrtc2);
-    g_signal_connect(webrtc2, "on-ice-candidate",
-                     G_CALLBACK(_on_ice_candidate), webrtc1);
+    g_signal_connect(webrtc2, "pad-added", G_CALLBACK(_webrtc_pad_added), pipe1);
+    g_signal_connect(webrtc1, "on-ice-candidate", G_CALLBACK(_on_ice_candidate), webrtc2);
+    g_signal_connect(webrtc2, "on-ice-candidate", G_CALLBACK(_on_ice_candidate), webrtc1);
 
     g_print("Starting pipeline\n");
     gst_element_set_state(GST_ELEMENT(pipe1), GST_STATE_PLAYING);
