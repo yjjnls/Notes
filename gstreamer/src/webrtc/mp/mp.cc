398ad939885de5b922bec57dc0da34cec113e058
yjjnls <x-jj@foxmail.com>
2018-07-18 15:29:10 +0800

    update

--- gstreamer/src/webrtc/mp/mp.cc
+++ gstreamer/src/webrtc/mp/mp.cc
@@ -1,7 +1,6 @@
 #include <gst/gst.h>
 #include <gst/sdp/sdp.h>
 
-#define GST_USE_UNSTABLE_API
 #include <gst/webrtc/webrtc.h>
 
 #include <vector>
@@ -18,8 +17,8 @@ class multipoint
     {
         video_tee_ = gst_element_factory_make("tee", "video-tee");
         audio_tee_ = gst_element_factory_make("tee", "audio-tee");
-        g_object_set(video_tee, "allow-not-linked", TRUE, NULL);
-        g_object_set(audio_tee, "allow-not-linked", TRUE, NULL);
+        g_object_set(video_tee_, "allow-not-linked", TRUE, NULL);
+        g_object_set(audio_tee_, "allow-not-linked", TRUE, NULL);
         video_input_selector_ = gst_element_factory_make("input-selector", "video-input-selector");
         audio_input_selector_ = gst_element_factory_make("input-selector", "audio-input-selector");
 
@@ -42,9 +41,9 @@ class multipoint
     }
     void generate_webrtc_enpoint(int num)
     {
-        char pattern[] = {'white', 'red', 'yellow', 'blue'};
+        std::string pattern[] = {"white", "red", "yellow", "blue"};
         for (int i = 0; i < num; ++i) {
-            std::string launch = "webrtcbin name=" + to_string(i) + " videotestsrc pattern=" + std::string(pattern[i]) + " ! videoconvert ! queue ! vp8enc deadline=1 ! rtpvp8pay ! queue ! " + RTP_CAPS_VP8 + "96 ! sendrecv. audiotestsrc wave=red-noise ! audioconvert ! audioresample ! queue ! alawenc ! rtppcmapay ! queue ! " + RTP_CAPS_OPUS + "8 ! sendrecv. ";
+            std::string launch = "webrtcbin name=" + std::to_string(i) + " videotestsrc pattern=" + pattern[i] + " ! videoconvert ! queue ! vp8enc deadline=1 ! rtpvp8pay ! queue ! " + RTP_CAPS_VP8 + "96 ! sendrecv. audiotestsrc wave=red-noise ! audioconvert ! audioresample ! queue ! alawenc ! rtppcmapay ! queue ! " + RTP_CAPS_OPUS + "8 ! sendrecv. ";
             printf("%s\n", launch.c_str());
         }
 
