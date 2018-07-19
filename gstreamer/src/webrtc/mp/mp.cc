b8a44852c1dfcf89102364232949330e6a9c4dec
yjjnls <x-jj@foxmail.com>
2018-07-19 09:07:46 +0800

    update

--- gstreamer/src/webrtc/mp/mp.cc
+++ gstreamer/src/webrtc/mp/mp.cc
@@ -1,7 +1,7 @@
 #include <gst/gst.h>
 #include <gst/sdp/sdp.h>
 
-#include <gst/webrtc/webrtc.h>
+#include <gst/remote_webrtc/remote_webrtc.h>
 
 #include <vector>
 #include <string>
@@ -22,32 +22,61 @@ class multipoint
         video_input_selector_ = gst_element_factory_make("input-selector", "video-input-selector");
         audio_input_selector_ = gst_element_factory_make("input-selector", "audio-input-selector");
 
-        pipeline_ = gst_pipeline_new("multipoint");
-        g_warn_if_fail(gst_bin_add(GST_BIN(pipeline_), video_tee_));
-        g_warn_if_fail(gst_bin_add(GST_BIN(pipeline_), audio_tee_));
-        g_warn_if_fail(gst_bin_add(GST_BIN(pipeline_), video_input_selector_));
-        g_warn_if_fail(gst_bin_add(GST_BIN(pipeline_), audio_input_selector_));
+        main_pipeline_ = gst_pipeline_new("multipoint");
+        g_warn_if_fail(gst_bin_add(GST_BIN(main_pipeline_), video_tee_));
+        g_warn_if_fail(gst_bin_add(GST_BIN(main_pipeline_), audio_tee_));
+        g_warn_if_fail(gst_bin_add(GST_BIN(main_pipeline_), video_input_selector_));
+        g_warn_if_fail(gst_bin_add(GST_BIN(main_pipeline_), audio_input_selector_));
 
         g_warn_if_fail(gst_element_link(video_input_selector_, video_tee_));
         g_warn_if_fail(gst_element_link(audio_input_selector_, audio_tee_));
     }
     ~multipoint()
     {
-        if (pipeline_) {
-            gst_element_set_state(pipeline_, GST_STATE_NULL);
-            g_object_unref(pipeline_);
-            pipeline_ = NULL;
+        if (main_pipeline_) {
+            gst_element_set_state(main_pipeline_, GST_STATE_NULL);
+            g_object_unref(main_pipeline_);
+            main_pipeline_ = NULL;
         }
     }
     void generate_webrtc_enpoint(int num)
     {
         std::string pattern[] = {"white", "red", "yellow", "blue"};
         for (int i = 0; i < num; ++i) {
-            std::string launch = "webrtcbin name=" + std::to_string(i) + " videotestsrc pattern=" + pattern[i] + " ! videoconvert ! queue ! vp8enc deadline=1 ! rtpvp8pay ! queue ! " + RTP_CAPS_VP8 + "96 ! sendrecv. audiotestsrc wave=red-noise ! audioconvert ! audioresample ! queue ! alawenc ! rtppcmapay ! queue ! " + RTP_CAPS_OPUS + "8 ! sendrecv. ";
+            std::string launch = "webrtcbin name=remote videotestsrc pattern=" + pattern[i] + " ! videoconvert ! queue ! vp8enc deadline=1 ! rtpvp8pay ! queue ! " + RTP_CAPS_VP8 + "96 ! sendrecv. audiotestsrc wave=red-noise ! audioconvert ! audioresample ! queue ! alawenc ! rtppcmapay ! queue ! " + RTP_CAPS_OPUS + "8 ! sendrecv. ";
+            
             printf("%s\n", launch.c_str());
+
+            GError *error = NULL;
+
+            GstElement *remote_pipe = gst_parse_launch(launch.c_str(), &error);
+
+            if (error) {
+                g_printerr("Failed to parse launch: %s\n", error->message);
+                g_error_free(error);
+                continue;
+            }
+
+
+            GstElement *remote_webrtc = gst_bin_get_by_name(GST_BIN(remote_pipe), to_string(i));
+            g_assert_nonnull(remote_webrtc);
+
+            /* This is the gstwebrtc entry point where we create the offer and so on. It
+            * will be called when the pipeline goes to PLAYING. */
+            g_signal_connect(remote_webrtc, "on-negotiation-needed", G_CALLBACK(on_negotiation_needed), NULL);
+            /* We need to transmit this ICE candidate to the browser via the websockets
+            * signalling server. Incoming ice candidates from the browser need to be
+            * added by us too, see on_server_message() */
+            g_signal_connect(remote_webrtc, "on-ice-candidate", G_CALLBACK(send_ice_candidate_message), NULL);
+            /* Incoming streams will be exposed via this signal */
+            g_signal_connect(remote_webrtc, "pad-added", G_CALLBACK(on_incoming_stream), remote_pipe);
+            /* Lifetime is the same as the pipeline itself */
+            gst_object_unref(remote_webrtc);
+
+            gst_element_set_state(GST_ELEMENT(remote_pipe), GST_STATE_PLAYING);
+            remote_pipelines_.push_back(remote_pipe);
         }
 
-        // gst_element_set_state(GST_ELEMENT(pipe1), GST_STATE_PLAYING);
     }
 
  private:
@@ -56,8 +85,9 @@ class multipoint
     GstElement *video_input_selector_;
     GstElement *audio_input_selector_;
 
-    GstElement *pipeline_;
-    std::vector<GstElement *> endpoints_;
+    GstElement *main_pipeline_;
+    std::vector<GstElement *> remote_pipelines_;
+    std::vector<GstElement *> local_pipelines_;
 };
 
 int main(int argc, char *argv[])
