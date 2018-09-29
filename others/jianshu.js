9f76d4cfe4fcaf74aaabebe44b73be2647877958
yjjnls <x-jj@foxmail.com>
2018-09-29 21:42:47 +0800

    update

--- others/jianshu.js
+++ others/jianshu.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
+const stringRandom = require('string-random');
 
-urls = ['https://www.jianshu.com/p/70f6a29a6296'];
+urls = ['https://www.jianshu.com/p/91c4178616fb', 'https://www.jianshu.com/p/a81194479b67', 'https://www.jianshu.com/p/19bbe6b7a73f', 'https://www.jianshu.com/p/ca569196dff6', 'https://www.jianshu.com/p/f82436325fc6', 'https://www.jianshu.com/p/5a89014c3345'];
 
 (async function test() {
     const browser = await puppeteer.launch();
@@ -8,10 +9,11 @@ urls = ['https://www.jianshu.com/p/70f6a29a6296'];
         const page = await browser.newPage();
         while (true) {
             for (var k = 0; k < 100; k++) {
-                for (var i = 0; i < urls.length; i += 1) {
-                    await page.goto(urls[i]);
-                    // sleep(500);
-                }
+                // for (var i = 0; i < urls.length; i += 1) {
+                //     await page.goto(urls[i]);
+                //     // sleep(500);
+                // }
+                await page.goto(urls[stringRandom(1, '012345')]);
             }
             await page.waitFor(1000);
         }
